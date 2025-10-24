3147e20a8249851b2062c914e2db0d79434e6290
Laysa <laysacristinemendes@gmail.com>
2025-10-24 20:56:48 -0300

    Add files via upload

---
+++ atividade 01/script.js
@@ -0,0 +1,12 @@
+console.log("testando o js!! hahahaha");
+
+const title =
+document.getElementById("title").
+    innerHTML = "é legal mexer com isso!";
+
+const button = 
+document.getElementById("button");
+    button.addEventListener("click", function()
+{
+    alert("isso ai!! o botão foi feito para ser clicado");
+})
---
+++ atividade 02/script.js
@@ -0,0 +1,18 @@
+const nome = "Laysa";
+console.log("nome:", nome);
+
+let idade = 20;
+console.log("sua idade era: ", idade);
+
+idade = 21;
+console.log("sua idade agora é: ", idade);
+
+const produto = {
+    nome: "Mouse",
+    valor: 50
+
+};
+console.log("produto inicial", produto);
+
+produto.valor = 30;
+console.log("produto apos alteração de valor", produto);
